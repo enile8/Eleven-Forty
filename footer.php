f4eab10904339e5ad586775e9fb1e3969433747e
Blake <email@enile8.org>
2011-10-12 17:44:24 -0400

    main theme files

---
+++ footer.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content
+ * after.  Calls sidebar-footer.php for bottom widgets.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+?>
+	</div><!-- #main -->
+
+	<div id="footer" class="row" role="contentinfo">
+		<div id="colophon">
+
+<?php
+	/* A sidebar in the footer? Yep. You can can customize
+	 * your footer with four columns of widgets.
+	 */
+	get_sidebar( 'footer' );
+?>
+
+			<div id="site-info" class="sixcol">
+				<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					<?php bloginfo( 'name' ); ?>
+				</a>
+			</div><!-- #site-info -->
+
+			<div id="site-generator" class="sixcol last">
+				<?php do_action( 'elevenforty_credits' ); ?>
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'elevenforty' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'elevenforty' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s.', 'elevenforty' ), 'WordPress' ); ?></a>
+			</div><!-- #site-generator -->
+
+		</div><!-- #colophon -->
+	</div><!-- #footer -->
+
+</div><!-- #wrapper -->
+
+<?php
+	/* Always have wp_footer() just before the closing </body>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to reference JavaScript files.
+	 */
+
+	wp_footer();
+?>
+</body>
+</html>
