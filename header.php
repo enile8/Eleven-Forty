b526bc8c16abbe7c6dbb6d67dece667666af10e8
Blake <email@enile8.org>
2011-10-14 22:27:59 -0300

    Update to php code that adds header image

--- header.php
+++ header.php
@@ -92,16 +92,16 @@
 					<div id="site-description" class="<?php echo get_option('of_site_description_class'); ?>"><?php bloginfo( 'description' ); ?></div>
 					<div id="header-image">
 					<?php
-						// Check if this is a post or page, if it has a thumbnail, and if it's a big one
-						if ( is_singular() &&
-								has_post_thumbnail( $post->ID ) &&
-								( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail' ) ) &&
-								$image[1] >= HEADER_IMAGE_WIDTH ) :
-							// Houston, we have a new header image!
-							echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
-						else : ?>
-							<img src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" />
-						<?php endif; ?>
+					// Check if this is a post or page, if it has a thumbnail, and if it's a big one
+					if ( is_singular() && current_theme_supports( 'post-thumbnails' ) &&
+							has_post_thumbnail( $post->ID ) &&
+							( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail' ) ) &&
+							$image[1] >= HEADER_IMAGE_WIDTH ) :
+						// Houston, we have a new header image!
+						echo get_the_post_thumbnail( $post->ID );
+					elseif ( get_header_image() ) : ?>
+						<img src="<?php header_image(); ?>" alt="" />
+					<?php endif; ?>
 					</div><!-- #header-image -->
 				</div><!-- #branding -->
 	</div><!-- .container -->
