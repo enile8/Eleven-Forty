2a8c5a9f2417b9ddf416d5270d578b5d37bf2158
Blake <email@enile8.org>
2011-10-28 14:41:53 -0400

    v0.3

--- library/extensions/shortcodes.php
+++ library/extensions/shortcodes.php
@@ -40,44 +40,5 @@ function elevenforty_copyright() {
 }
 add_shortcode('copyright', 'elevenforty_copyright');
 
-// Providing information about Thematic
-
-function theme_name() {
-    return THEMENAME;
-}
-add_shortcode('theme-name', 'theme_name');
-
-function theme_author() {
-    return THEMEAUTHOR;
-}
-add_shortcode('theme-author', 'theme_author');
-
-function theme_uri() {
-    return THEMEURI;
-}
-add_shortcode('theme-uri', 'theme_uri');
-
-
-// Providing information about the child theme
-
-function child_name() {
-    return TEMPLATENAME;
-}
-add_shortcode('child-name', 'child_name');
-
-function child_author() {
-    return TEMPLATEAUTHOR;
-}
-add_shortcode('child-author', 'child_author');
-
-function child_uri() {
-    return TEMPLATEURI;
-}
-add_shortcode('child-uri', 'child_uri');
-
-function child_version() {
-    return TEMPLATEVERSION;
-}
-add_shortcode('child-version', 'child_version');
 
 ?>
